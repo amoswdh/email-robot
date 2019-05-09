1a6211f35b8fc24b4f26ff400470a73a30a9a986
amos <752711024@qq.com>
2019-05-09 10:14:52 +0800

    fix

---
+++ migrations/2019_05_09_020906_create_email_log_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_log', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('header');
+            $table->text('body');
+            $table->integer('mail_id');
+            $table->integer('account_id');
+            $table->tinyInteger('status');
+            $table->timestamp('created_at');
+            $table->dateTime('updated_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('email_log');
+    }
+}
